f97785c92eda5640f82f1835b1ce750b3a0b7f68
ibordea <ibordea@griddynamics.com>
2025-06-10 09:14:32 +0300

    module 1 react file to html added

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.ts',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	devtool: 'source-map',
-	resolve: {
-		extensions: ['.ts', '.js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: 'babel-loader',
-			},
-		],
-	},
-	devServer: {
-		static: './dist',
-		open: true,
-		port: 3000,
-	},
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
